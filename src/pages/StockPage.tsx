2e306694c906ccbf6f1224f73285ef5a7e4c5692
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-19 01:47:19 +0530

    stock page implement

---
+++ src/pages/StockPage.tsx
@@ -0,0 +1,41 @@
+import {motion} from "framer-motion";
+import {useState} from "react";
+
+export function StockPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    return (
+        <motion.div
+            initial={{
+                opacity: 0,  // Start invisible
+            }}
+            animate={{
+                opacity: 1,  // Fade in to full visibility
+            }}
+            exit={{
+                opacity: 0,  // Fade out
+            }}
+            transition={{
+                duration: 0.8,  // Duration for the fade effect
+                ease: [0.25, 0.8, 0.5, 1],  // Smooth easing curve
+            }}
+        >
+            <div className="container mx-auto p-5">
+                <h1 className="text-xl sm:text-2xl font-semibold mb-8 text-center sm:text-left">
+                    Stock Management
+                </h1>
+
+                <div className="flex flex-wrap justify-end sm:justify-end space-x-0 sm:space-x-4 mb-5">
+                    <button
+                        id="btn-add"
+                        onClick={() => setIsModalOpen(true)}
+                        className="bg-green-600 text-white px-8 py-2 rounded-lg font-semibold hover:bg-green-700 transition text-sm sm:text-base flex items-center space-x-2 group sm:w-auto"
+                    >
+                        <i className="fa-solid fa-plus font-bold"></i>
+                        <span className="pl-2">Add</span>
+                    </button>
+                </div>
+            </div>
+        </motion.div>
+    );
+}
